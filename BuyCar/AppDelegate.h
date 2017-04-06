e6e40b1493af899e5b8f19adacd6a2f1adf64a95
Zoombie <a13530836164@163.com>
2017-04-06 09:50:02 +0800

    Initial Commit

---
+++ BuyCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuyCar
+//
+//  Created by xfej on 17/4/6.
+//  Copyright © 2017年 消费e家. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuyCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuyCar
+//
+//  Created by xfej on 17/4/6.
+//  Copyright © 2017年 消费e家. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
